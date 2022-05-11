2ab8f99b82058d2f4799439520e00b48ca76f108
Teneroy <alexchernishof@gmail.com>
2022-05-11 03:59:28 +0200

    Learned if statement, loops

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -15,28 +18,42 @@ func main() {
 
 	var bookings []string //Slice
 
-	var userName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for their name
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&userName)
+	for {
+		var userName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&userName)
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Print("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, userName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v booked %v tickets. You will receive a confirmation on %v\n", userName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v booked %v tickets. You will receive a confirmation on %v\n", userName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		firstNames := []string{}
+
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("First names of our bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			//end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			break
+		}
+	}
 }
